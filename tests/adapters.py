df316b1cc811ed436ffa15ece6f4c88f11be8adb
jinfenglin <ljfnwpusife@gmail.com>
2026-02-01 00:40:22 -0800

    linear layer implementation

--- tests/adapters.py
+++ tests/adapters.py
@@ -8,6 +8,7 @@ import numpy.typing as npt
 import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
+from transformer.linear import Linear
 
 
 def run_linear(
@@ -29,7 +30,9 @@ def run_linear(
         Float[Tensor, "... d_out"]: The transformed output of your linear module.
     """
 
-    raise NotImplementedError
+    linear = Linear(d_in, d_out)
+    linear.load_state_dict({'w': weights})
+    return linear(in_features)
 
 
 def run_embedding(
